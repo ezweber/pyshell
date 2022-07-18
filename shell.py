c872816f7e542a042680235e52969b92a7625d6e
ezweber <83794833+ezweber@users.noreply.github.com>
2022-07-17 22:42:31 -0500

    Used fstrings and added comments

--- shell.py
+++ shell.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
+
 import os
 import subprocess
 import socket
 
+# Quits the shell
 def quit_sh():
     print("Goodbye!")
     quit()
 
+# Prints a help menu
 def help():
     x = """
         Pyshell 1.0.1
@@ -24,30 +28,35 @@ def help():
         """
     print(x)
 
+# Runs outside shell commands using subprocesses
 def subpro(command):
     try:
         subprocess.run(command[1])
     except FileNotFoundError:
         print("That command does not exist.")
 
+# Makes a new directory
 def mkdir(command):
     try:
         os.mkdir(command[1])
     except IndexError:
         print("ERROR: You probably forgot the dir name.")
 
+# Deletes a directory
 def rmdir(command):
     try:
         os.rmdir(command[1])
     except IndexError:
         print("ERROR: You probably forgot the dir name.")
 
+# Prints whatever text you put after it
 def echo(command):
     try:
         print(command[1])
     except IndexError:
         print("ERROR: You need something to echo.")
 
+# Makes a file then asks for input
 def mkfile(command):
     try:
         with open(command[1], "w") as file:
@@ -55,12 +64,14 @@ def mkfile(command):
     except:
         print("ERROR: Did you specify a file name?")
 
+# Deletes a file
 def rmfile(command):
     try:
         os.remove(command[1])
     except:
         print("ERROR: Did you type the file name correctly?")
 
+# Prints the file you give it
 def pfile(command):
     try:
        with open(command[1], "r") as file:
@@ -68,6 +79,7 @@ def pfile(command):
     except:
         print("ERROR: Does that file exist?")
 
+# Changes the working directory
 def cd(command):
     try:
         os.chdir(command[1])
@@ -76,17 +88,21 @@ def cd(command):
     except NotADirectoryError:
         print("ERROR: That is not a directory")
 
+# Lists all files and directories in the current working directory
 def ls():
     print(*os.listdir("./"), sep = "\n")
 
+# Clears the screen
 def clear():
     for x in range(100):
         print("\n")
     
+# Sets the current working directory to the users home at startup
 os.chdir(os.environ['HOME'])
 
 while True:
-    command = str(input("[" + "\033[92m" + socket.gethostname() + " " + os.getcwd() + "\033[00m" + "] ")).split()
+    # Gets the command and displays the hostname and path
+    command = str(input(f"[\033[92m{socket.gethostname()} {os.getcwd()}\033[00m] ")).split()
 
     if(command[0] == "quit"):
         quit_sh()
