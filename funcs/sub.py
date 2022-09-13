327f85c6d33c81884fa1af4d4f40089e5ed61d00
ezweber <83794833+ezweber@users.noreply.github.com>
2022-09-12 22:20:49 -0500

    You can now add args to commands run with "sub"

--- funcs/sub.py
+++ funcs/sub.py
@@ -1,4 +1,10 @@
+del cmd[0]
+
+#sub_cmd = ' '.join(cmd)
+
+#print(sub_cmd)
+
 try:
-    subprocess.run(cmd[1])
+    subprocess.run(cmd)
 except FileNotFoundError:
     print("That command does not exist.")
